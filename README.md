f20a130e1f9de10140adbbbddffa8067dad6c8b6
jbouwh <jan@jbsoft.nl>
2025-04-14 20:25:58 +0000

    Initial release

--- README.md
+++ README.md
@@ -1,2 +1,88 @@
-# mailjet-state-reporter
+# Mailjet state reporter
+
 Process transactional mail logs and send a daily delivery report
+
+## Daily bounce reports
+
+The Mailjet state reporter script helps to automate sending a boubce and statistic report to about sent emails that were not delivered as expected.
+
+## Environment setup
+
+The script requires the following environment variables to be set:
+
+```bash
+MAILJET_APP_ID=xxxx # The master API ID
+MAILJET_APP_SECRET=xxxx # The master API key
+CONFIG_FILE=config/settings.yaml # The location of the YAML settings file
+SYNC_STATE=config/sync_state.json # The location where the last processed timestamp is stored
+```
+
+## settings.yaml
+
+Example settings file:
+
+```yaml
+# This file contains configuration settings for the Mailjet State Reporter application.
+# Modify these settings as needed to customize the application's behavior.
+
+status_translations:
+  softbounced: "Soft bounced"
+  hardbounced: "Hard bounced"
+  unsub: "Unsubscribed"
+  spam: "Marked as SPAM"
+  queued: "In Queue"
+  sent: "Sent"
+  opened: "Opened"
+  clicked: "Clicked"
+  blocked: "Blocked"
+  no_data: "No data for period"
+  date_time: "Date/time"
+  contact: "To"
+  state: "State"
+  subject: "Subject"
+
+global_settings:
+  report_days: "12345" # 1=monday, 2=tuesday, 3=wednesday, 4=thursday, 5=friday, 6=saturday, 7=sunday
+  default_max_report_days: 60 # The maximum number of days to report (first time only)
+  timezone: "Europe/Amsterdam"
+  time_format: "%Y-%m-%d %H:%M"
+
+profiles:
+  relay:
+    template_id: 1234567 # The mailjet template to send the status report with
+    from_email: "noreply-bounce-service@example.com"
+    from_name: "Example Bounce Service"
+    subject: "Example Bounce Repport"
+    report_days: "12345" # 1=monday, 2=tuesday, 3=wednesday, 4=thursday, 5=friday, 6=saturday, 7=sunday
+    report_in_detail:
+      - "blocked"
+      - "softbounced"
+      - "hardbounced"
+      - "unsub"
+      - "spam"
+      - "queued"
+
+subaccount_reports:
+  - name: "SUBACCOUNT TEST" # Name of the Mailjet subaccount
+    profile: "relay" # Valid defined profile key
+    recipients:
+      - to_email: "john.doe@example.com" # Email address to send the report to
+        to_name: "John Doe" # Name to send the report to
+```
+
+## Running the script
+
+Run the script once a day on a fixed time, e.g. at 6 AM. If the current day is not in `report_days` the reporting will be skipped.
+
+The script will save a timestamp of the last runtime for each subaccount.
+
+## Template for sending the reports
+
+The `template_id` is the ID for a transactional template on the master account.
+The template can use the following variables:
+
+- `rep_start`: Field for the start time of the report.
+- `rep_end`: Field for the end time of the report.
+- `sub_account`: Field for the name of the subaccount.
+- `delivery_stats`: Field to hold the statistics (HTML).
+- `bounce_data`: Field to hold the bounce data (HTML). This will include details on the states defined in the `report_in_detail` profile setting.
---
+++ mailjet_state_reporter/__init__.py
@@ -0,0 +1,451 @@
+"""Mailjet mail status reporter."""
+
+import logging
+import os
+import sys
+from time import time
+
+from typing import Any
+
+from datetime import datetime
+import json
+from zoneinfo import ZoneInfo
+import requests
+from requests.auth import HTTPBasicAuth
+import yaml
+
+
+_LOGGER = logging.getLogger(__name__)
+
+MAILJET_APP_ID = os.environ.get("MAILJET_APP_ID")
+MAILJET_APP_SECRET = os.environ.get("MAILJET_APP_SECRET")
+CONFIG_FILE = os.environ.get("CONFIG_FILE")
+SYNC_STATE = os.environ.get("SYNC_STATE")
+
+MAILJET_BASE_URL = "https://api.mailjet.com"
+MAILJET_MESSAGE_API = f"{MAILJET_BASE_URL}/v3/REST/message"
+MAILJET_APIKEY_API = f"{MAILJET_BASE_URL}/v3/REST/apikey"
+MAILJET_MAIL_SEND_API = f"{MAILJET_BASE_URL}/v3.1/send"
+
+BATCH_LIMIT = 200
+
+MESSAGE_FIELDS = {
+    "ID": "id",
+    "ArrivedAt": "date_time",
+    "ContactAlt": "contact",
+    "Status": "state",
+    "Subject": "subject",
+}
+
+REPORT_DETAILS_FIELDS = [
+    "date_time",
+    "contact",
+    "subject",
+]
+
+
+def get_mailjet_data_list(
+    api: str, auth: HTTPBasicAuth, params: dict[str, Any] | None = None
+) -> list[dict[str, Any]] | None:
+    """Fetch a data from the mailjet API."""
+    if params is None:
+        params = {}
+    query_params = {"countOnly": 1} | params
+    try:
+        response = requests.get(api, auth=auth, params=query_params, timeout=10)
+        count = json.loads(response.content)["Count"]
+    except (
+        KeyError,
+        json.JSONDecodeError,
+        requests.exceptions.RequestException,
+    ) as err:
+        _LOGGER.error("Error fetching count from API %s: %s", api, err)
+        return None
+
+    offset = 0
+    data = []
+    while True:
+        query_params = {"Limit": BATCH_LIMIT, "Offset": offset} | params
+        response = requests.get(api, auth=auth, params=query_params, timeout=10)
+        batch = json.loads(response.content)
+        batch_count = batch["Count"]
+        data.extend(batch["Data"])
+        offset += batch_count
+        if not batch_count or offset >= count:
+            break
+    return data
+
+
+def get_subaccount_data(auth: HTTPBasicAuth) -> dict[str, dict[str, str]] | None:
+    """Get subaccount data."""
+    if (
+        subaccount_api_data := get_mailjet_data_list(MAILJET_APIKEY_API, auth=auth)
+    ) is None:
+        _LOGGER.error("Error fetching subaccount data")
+        return None
+    return {
+        subaccount["Name"]: {
+            "id": subaccount["ID"],
+            "api_id": subaccount["APIKey"],
+            "api_secret": subaccount["SecretKey"],
+        }
+        for subaccount in subaccount_api_data
+    }
+
+
+def time_from_timestamp(timestamp: int, global_settings: dict[str, Any]) -> str:
+    """Convert a timestamp to a formatted string."""
+    timezone = global_settings.get("timezone", "Europe/Amsterdam")
+    time_format = global_settings.get("time_format", "%Y-%m-%d %H:%M:%S")
+    return (
+        datetime.fromtimestamp(timestamp)
+        .astimezone(ZoneInfo(timezone))
+        .strftime(time_format)
+    )
+
+
+def time_from_iso_format(date_time: str, global_settings: dict[str, Any]) -> str:
+    """Convert a timestamp to a formatted string."""
+    timezone = global_settings.get("timezone", "Europe/Amsterdam")
+    time_format = global_settings.get("time_format", "%Y-%m-%d %H:%M:%S")
+    return (
+        datetime.fromisoformat(date_time)
+        .astimezone(ZoneInfo(timezone))
+        .strftime(time_format)
+    )
+
+
+def gen_message_stats_html(
+    config: dict[str, Any], message_stats: dict[str, int]
+) -> str:
+    """Generate HTML for the message stats."""
+    status_translations: dict[str, Any] = config.get("status_translations", {})
+    delivery_stats_html = "<table>"
+    for status, count in message_stats.items():
+        header = status_translations.get(status, status)
+        delivery_stats_html += f"<tr><td>{header}</td><td>{count}</td></tr>"
+    delivery_stats_html += "</table>"
+    return delivery_stats_html
+
+
+def gen_bounce_data_html(
+    config: dict[str, Any], message_details: dict[str, list[dict[str, Any]]]
+) -> str:
+    """Generate HTML for the bounce data."""
+    status_translations: dict[str, Any] = config.get("status_translations", {})
+    bounce_data_html = ""
+    for state, status_messages in message_details.items():
+        bounce_data_html += f"<h4>{status_translations.get(state, state)}:</h4>"
+        bounce_data_html += "<table><tr>"
+        for field in REPORT_DETAILS_FIELDS:
+            bounce_data_html += f"<th>{status_translations.get(field, field)}</th>"
+        bounce_data_html += "</tr>"
+        for message in status_messages:
+            bounce_data_html += "<tr>"
+            for field in REPORT_DETAILS_FIELDS:
+                content = (
+                    time_from_iso_format(
+                        message[field], config.get("global_settings", {})
+                    )
+                    if field == "date_time"
+                    else message[field]
+                )
+                bounce_data_html += f"<td>{content}</td>"
+            bounce_data_html += "</tr>"
+
+        bounce_data_html += "</table>"
+
+    return bounce_data_html or "undefined"
+
+
+def send_report(
+    config: dict[str, Any],
+    subaccount: dict[str, Any],
+    message_stats: dict[str, int],
+    message_details: dict[str, list[dict[str, Any]]],
+    last_ts: int,
+    current_ts: int,
+    auth: HTTPBasicAuth,
+) -> bool:
+    """Send the report for the subaccount."""
+    global_settings: dict[str, Any] = config.get("global_settings", {})
+    status_translations: dict[str, Any] = config.get("status_translations", {})
+    profile_details = subaccount["profile_details"]
+    delivery_stats_html = (
+        status_translations.get("no_data", "No data")
+        if not message_stats
+        else gen_message_stats_html(config, message_stats)
+    )
+    message_body = {
+        "Messages": [
+            {
+                "From": {
+                    "Email": profile_details["from_email"],
+                    "Name": profile_details["from_name"],
+                },
+                "To": [
+                    {"Email": recipient["to_email"], "Name": recipient["to_name"]}
+                    for recipient in subaccount["recipients"]
+                ],
+                "TemplateID": profile_details["template_id"],
+                "TemplateLanguage": True,
+                "Subject": profile_details["subject"],
+                "Variables": {
+                    "delivery_stats": delivery_stats_html,
+                    "bounce_data": gen_bounce_data_html(config, message_details),
+                    "rep_start": time_from_timestamp(last_ts, global_settings),
+                    "rep_end": time_from_timestamp(current_ts, global_settings),
+                    "sub_account": subaccount["name"],
+                },
+            }
+        ]
+    }
+    headers = {"Content-Type": "application/json"}
+    try:
+        response = requests.post(
+            MAILJET_MAIL_SEND_API,
+            data=json.dumps(message_body),
+            auth=auth,
+            headers=headers,
+            timeout=10,
+        )
+        if response.status_code != 200:
+            _LOGGER.error(
+                "Error sending report for subaccount %s: %s",
+                subaccount["name"],
+                response.status_code,
+            )
+            return False
+    except (requests.exceptions.RequestException,) as err:
+        _LOGGER.error(
+            "Error fetching count from API %s: %s", MAILJET_MAIL_SEND_API, err
+        )
+        return False
+
+    return True
+
+
+def main() -> None:
+    """Main function to run the script."""
+    day_of_week = str(datetime.now().isoweekday())
+    config: dict[str, Any] | None
+
+    if MAILJET_APP_ID is None or MAILJET_APP_SECRET is None:
+        _LOGGER.error("MAILJET_APP_ID or MAILJET_APP_SECRET is not set")
+        sys.exit(1)
+
+    if CONFIG_FILE is None or SYNC_STATE is None:
+        _LOGGER.error("CONFIG_FILE or SYNC_STATE is not set")
+        sys.exit(1)
+
+    try:
+        with open(CONFIG_FILE, "r", encoding="utf-8") as config_file:
+            config = yaml.safe_load(config_file)
+            if config is None:
+                _LOGGER.error("Config file is empty, aborting")
+                sys.exit(1)
+    except FileNotFoundError:
+        _LOGGER.error("Config file %s not found, aborting", CONFIG_FILE)
+        sys.exit(1)
+
+    global_settings: dict[str, Any] = config.get("global_settings", {})
+    report_days_default: str = global_settings.get("report_days", "01234")
+    default_max_report_days: int = global_settings.get("default_max_report_days", 1)
+
+    state: dict[int, int]
+    try:
+        with open(SYNC_STATE, "r", encoding="utf-8") as state_file:
+            state = json.load(state_file)
+        if state is None:
+            _LOGGER.info(
+                "State file is empty, reporting over the last %s days",
+                default_max_report_days,
+            )
+    except FileNotFoundError:
+        _LOGGER.debug("State file %s not found, creating initial file", SYNC_STATE)
+        state = {}
+
+    profiles: dict[str, dict[str, Any]] = config.get("profiles", {})
+    if not profiles:
+        _LOGGER.error("No profiles found in config file")
+        sys.exit(1)
+
+    current_ts = int(time())
+    default_last_ts = current_ts - 86400 * default_max_report_days
+
+    for profile_name, profile in profiles.items():
+        if "template_id" not in profile:
+            _LOGGER.error(
+                "Profile %s does not have a template_id, skipping", profile_name
+            )
+            continue
+        if "subject" not in profile:
+            _LOGGER.error(
+                "Profile %s does not have a subject set, skipping", profile_name
+            )
+            continue
+        if "from_email" not in profile:
+            _LOGGER.error(
+                "Profile %s does not have a from_email, skipping", profile_name
+            )
+            continue
+        if "from_name" not in profile:
+            _LOGGER.error(
+                "Profile %s does not have a from_name, skipping", profile_name
+            )
+            continue
+        profile["valid"] = True
+
+    subaccount_reports: list[dict[str, Any]] = config.get("subaccount_reports", [])
+    if not subaccount_reports:
+        _LOGGER.info("No subaccount reports found in config file, nothing to report")
+        sys.exit(1)
+
+    # Validate subaccount config
+    for subaccount in subaccount_reports:
+        if "name" not in subaccount:
+            _LOGGER.error(
+                "Subaccount name not found in subaccount report config, skipping"
+            )
+            continue
+        if "profile" not in subaccount:
+            _LOGGER.error(
+                "Subaccount '%s' does not have a profile assigned, skipping",
+                subaccount["name"],
+            )
+            continue
+        if subaccount["profile"] not in profiles:
+            _LOGGER.error(
+                "Assigned profile '%s' for subaccount '%s' does not exist, skipping",
+                subaccount["profile"],
+                subaccount["name"],
+            )
+            continue
+        if subaccount.get("profile") not in profiles or not profiles[
+            subaccount["profile"]
+        ].get("valid", False):
+            _LOGGER.error(
+                "Subaccount type %s not found in profiles, skipping", subaccount["name"]
+            )
+            continue
+        subaccount["profile_details"] = profiles[subaccount["profile"]]
+        subaccount["report_days"] = subaccount.get("report_days", report_days_default)
+        if "recipients" not in subaccount:
+            _LOGGER.error(
+                "Subaccount %s does not have recipients, skipping", subaccount["name"]
+            )
+            continue
+        invalid_recipient = False
+        for recipient in subaccount["recipients"]:
+            if "to_email" not in recipient or "to_name" not in recipient:
+                _LOGGER.error(
+                    "Subaccount %s recipients has not defined "
+                    "to_email or to_name correctly, skipping",
+                    subaccount["name"],
+                )
+                invalid_recipient = True
+                continue
+
+        if invalid_recipient:
+            continue
+
+        subaccount["valid"] = True
+
+    # Get API keys and check subaccount validity
+    master_auth = HTTPBasicAuth(MAILJET_APP_ID, MAILJET_APP_SECRET)
+    if (subaccount_data := get_subaccount_data(auth=master_auth)) is None:
+        _LOGGER.error("Error fetching subaccounts, aborting")
+        sys.exit(1)
+
+    # Process subaccount reports
+    for subaccount in subaccount_reports:
+        if subaccount.get("valid", False) is False:
+            _LOGGER.warning("Subaccount %s not valid, skipping", subaccount["name"])
+            continue
+        if day_of_week not in subaccount["report_days"]:
+            _LOGGER.debug(
+                "Subaccount report %s not scheduled to report today, skipping",
+                subaccount["name"],
+            )
+            continue
+        subaccount_message_details: dict[str, list[dict[str, Any]]] = {}
+        subaccount_message_stats: dict[str, int] = {}
+        if subaccount["name"] not in subaccount_data:
+            _LOGGER.error(
+                "Subaccount %s not found in Mailjet, skipping", subaccount["name"]
+            )
+            continue
+        api_id = subaccount_data[subaccount["name"]]["api_id"]
+        api_secret = subaccount_data[subaccount["name"]]["api_secret"]
+        subaccount_id = str(subaccount_data[subaccount["name"]]["id"])
+        auth = HTTPBasicAuth(api_id, api_secret)
+        last_ts = state.get(subaccount_id, default_last_ts)
+        report_details_fields: set[str] = set(
+            profiles[subaccount["profile"]].get("report_in_detail", [])
+        )
+
+        if (
+            subaccount_message_data := get_mailjet_data_list(
+                MAILJET_MESSAGE_API,
+                auth=auth,
+                params={
+                    "FromTS": last_ts,
+                    "ToTS": current_ts,
+                    "ShowSubject": True,
+                    "ShowContactAlt": True,
+                },
+            )
+        ) is None:
+            _LOGGER.error(
+                "Error fetching message data for subaccount %s, skipping",
+                subaccount["name"],
+            )
+            continue
+
+        # Process message data
+        for message in subaccount_message_data:
+            if (message_status := message["Status"]) in report_details_fields:
+                subaccount_message_details.setdefault(message_status, []).append(
+                    {
+                        header: message[field]
+                        for field, header in MESSAGE_FIELDS.items()
+                        if field in message
+                    }
+                )
+            subaccount_message_stats[message_status] = (
+                subaccount_message_stats.get(message_status, 0) + 1
+            )
+
+        # Send report
+        if not send_report(
+            config,
+            subaccount,
+            subaccount_message_stats,
+            subaccount_message_details,
+            last_ts,
+            current_ts,
+            auth,
+        ):
+            continue
+
+        # update state to current timestamp
+        state[subaccount_id] = current_ts
+
+    # Write state to file
+    with open(SYNC_STATE, "w", encoding="utf-8") as state_file:
+        json.dump(state, state_file)
+        # Ensure the file is written to disk
+        state_file.flush()
+        os.fsync(state_file.fileno())  # type: ignore[arg-type]
+
+
+if __name__ == "__main__":
+    logging.basicConfig(
+        format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
+        level=logging.INFO,
+        handlers=[
+            logging.StreamHandler(sys.stdout),
+            logging.FileHandler("mailjet_state_reporter.log"),
+        ],
+    )
+    main()
